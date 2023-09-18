4a03f8463bd8cf06a535eed98d9f418a55e4076e
PatataAcustica <100472783@alumnos.uc3m.es>
2023-09-18 19:08:15 +0200

    added formatted date, but doesn't show hour though

--- timeClient.py
+++ timeClient.py
@@ -2,14 +2,19 @@
 
 import sys
 import socket
+from datetime import date, time
 
 CSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 CSocket.connect(("utcnist.colorado.edu", 37))
 #The default port to get time from a server is 37, specify in program arguments using -p if a different one is wanted
 time = CSocket.recv(1024)
-if not time:
+if time == None:
     print("TCP connection not working")
 else:
-    timeInt = int.from_bytes(time,"big")
-    print(timeInt)
+    timestamp = int.from_bytes(time,"big")
+    print(timestamp)
+    ahora = date.fromtimestamp((timestamp-2208988800))
+    
+    formatdate = ahora.strftime("%a %b %d {0}:%M:%S %Y".format(ahora.hour))
+    print(formatdate)
 CSocket.close()
