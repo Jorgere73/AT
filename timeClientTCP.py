d085d6e260cb75776a2e9a60204414dec206d4bb
PatataAcustica <100472783@alumnos.uc3m.es>
2023-09-18 20:03:33 +0200

    File renamed

--- timeClientTCP.py
+++ timeClientTCP.py
@@ -2,19 +2,23 @@
 
 import sys
 import socket
-from datetime import date, time
+from datetime import datetime
+
+SETENTA = 2208988800
+#Represents the time in seconds equivalent to seventy years time
+
 
 CSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 CSocket.connect(("utcnist.colorado.edu", 37))
 #The default port to get time from a server is 37, specify in program arguments using -p if a different one is wanted
 time = CSocket.recv(1024)
+#The timestamp is given in bytes type
 if time == None:
     print("TCP connection not working")
 else:
-    timestamp = int.from_bytes(time,"big")
-    print(timestamp)
-    ahora = date.fromtimestamp((timestamp-2208988800))
-    
-    formatdate = ahora.strftime("%a %b %d {0}:%M:%S %Y".format(ahora.hour))
+    timestamp = int.from_bytes(time,"big") 
+    #Converts the bytes pseudotimestamp into an integer using big endian order
+    ahora = datetime.fromtimestamp((timestamp-SETENTA))
+    formatdate = ahora.strftime("%a %b %d %H:%M:%S %Y")
     print(formatdate)
 CSocket.close()
