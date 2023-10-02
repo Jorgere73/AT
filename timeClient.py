b1ff573072aa1b8905a248939eb7108a73023142
PatataAcustica <100472783@alumnos.uc3m.es>
2023-10-02 11:25:48 +0200

    Commented  code

--- timeClient.py
+++ timeClient.py
@@ -6,13 +6,13 @@ from datetime import datetime
 import time as t
 
 SETENTA = 2208988800
-#Represents the time in seconds equivalent to seventy years time
+#Tiempo en segundos equivalente a setenta años
 
-
+#Funcion que devuelve la IP local del host 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
+        # ip de prueba, no tiene por qué ser ni accesible
         s.connect(('192.255.255.255', 1))
         IP = s.getsockname()[0]
     except:
@@ -24,9 +24,10 @@ def get_local_ip():
 
 def formatDate(input):
     timestamp = int.from_bytes(input,"big") 
-    #Converts the bytes pseudotimestamp into an integer using big endian order
+    #Convierte los bytes del timestamp a int en orden big endian
     ahora = datetime.fromtimestamp((timestamp-SETENTA))
     formatdate = ahora.strftime("%a %b %d %H:%M:%S %Y")
+    #Da forma al output de la hora
     print(formatdate)
 
 def getDate():
@@ -41,23 +42,29 @@ def getDate():
 
 #-------------------------------------------------------------------
 
+#Funcion que procesa el sistema de hora desde parte del cliente
 def clientMode(mode, servername, port = 37):
     if(servername == ""):
-        print("Server name not specified, set using -s")
+        print("Nombre de servidor no especificado, usar con -s")
         sys.exit()
     if(mode == "tcp"):
         try:
             CSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #Crea socket TCP
             CSocket.connect((serverName, port))
+            #Conecta al servidor y puerto pasados por parametro
+            #Usamos el while para que reciba el tiempo del servidor constantemente, funcion pedida en el enunciado
             while True:
                 #The default port to get time from a server is 37, specify in program arguments using -p if a different one is wanted
                 #print("1")
                 time = CSocket.recv(1024)
-                #The timestamp is given in bytes type
-                if time == None or time == 0:
-                    print("Connection not working")
+                #Recibe datos del servidor al que se ha conectado
+                if time == None or time == b'':
+                    #Si no ha devuelto ninguna hora
+                    print("Conexión no funciona o ha sido cerrada")
                     break
                 else:
+                    #Si el programa ha funcionado sin problema
                     formatDate(time)
                 
         except KeyboardInterrupt:
@@ -66,13 +73,18 @@ def clientMode(mode, servername, port = 37):
             print("No se ha podido conectar al servidor")
         CSocket.close()
     else:
+        #Si el modo es UDP
         try:
             CSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            #Create UDP socket 
+            #Creamos socket UDP
             CSocket.sendto("".encode(), (serverName, port))
+            #Enviamos un paquete vacio al servidor para que nos pueda responder con la hora
             time, address = CSocket.recvfrom(1024)
+            #Recibimos los datos enviados desde el servidor
             formatDate(time)
+            #Damos forma a la hora y tiempo
         except:
+            #Si no se recibe respuesta, muestra mensaje alternativo
             print("El servidor al que intenta contactar no responde, compruebe la direccion y puerto introducido")
         CSocket.close()
         
@@ -81,35 +93,46 @@ def clientMode(mode, servername, port = 37):
 #--------------------------------------------------------------------
 
 def serverMode(portnum):
-    ip = get_local_ip()
-    print("Servidor establecido en " + str(ip) + ", en puerto " + str(portnum))
-    SSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    SSocket.bind((ip, portnum))
-    #Bind server ip address to port
-    SSocket.listen(5)
-    (clientx, clientAddr) = SSocket.accept()
-    while True:
-        try:
-            timestamp = getDate()
-            print("1")
-            clientx.send(getDate())
-            t.sleep(1)
-        except KeyboardInterrupt:
-            print("SIGINT: closing server")
-            SSocket.close()
-            sys.exit(0)
-
+    try:
+        ip = get_local_ip()
+        #Conseguimos nuestra ip de host
+        print("Servidor establecido en " + str(ip) + ", en puerto " + str(portnum))
+        SSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Creamos socket TCP
+        SSocket.bind((ip, portnum))
+        #Bind server ip address to port
+        SSocket.listen(5)
+        #Habilita al servidor para atender a cinco clientes a la vez
+        (clientx, clientAddr) = SSocket.accept()
+        #Acepta conexiones de clientes al servidor 
+        while True:
+            try:
+                timestamp = getDate()
+                #Transforma la hora local en un timestamp
+                clientx.send(getDate())
+                #Envia el timestamp al cliente a traves de la conexion abierta
+                t.sleep(1)
+                #Espera un segundo antes de volver a enviar
+            except KeyboardInterrupt:
+                print("SIGINT: closing server")
+                #Cierra el servidor si se envia un SIGINT
+                SSocket.close()
+                sys.exit(0)
+    except:
+        print("Puerto en uso")
+        #Si el puerto seleccionado esta en uso, salta el except
+        sys.exit(1)
 
 i = 1
-#numerical counter for loop
+#Contador para el loop
 no = len(sys.argv) - 1
-#Number of arguments
+#Cuenta el numero de argumentos proporcionados al programa
 serverName = ""
-#Name of the server we're going to connect to
+#Nombre del servidor al que nos vamos a conectar
 mode = "udp"
-#Protocol used for the communication
+#Protocolo que usamos para la comunicacion
 portn = 0
-#Number for the selected port (server or client)
+#Numero de puerto que vamos a usar (server o client)
 
 for arg in sys.argv[1:]:
     if(arg == "-s" and i < no):
@@ -122,20 +145,22 @@ for arg in sys.argv[1:]:
         elif(sys.argv[i+1] == "s"):
             mode = "server"
         else:
-            print("Usage is -m [cu] [ct] [s]")
+            print("Forma: -m [cu] [ct] [s]")
             sys.exit()
     elif(arg == "-p" and i < no):
         try:
             portstr = sys.argv[i+1]
             portnum = int(portstr)
             if(portnum < 0 or portnum > 65535):
-                raise Exception("Port number out of bounds")
+                raise Exception("Numero de puerto fuera de rango")
         except:
-            print("Introduce an available port number, 0-65.535")
+            print("Introduzca un numero de puerto válido, 0-65.535")
             sys.exit()
         portn = portnum
     i+=1
-
+if(portn == 0):
+    portn = 37
+    #Para que puerto 37 sea el default (si no se le da ningun valor)
 if(mode == "server"):
     #serverMode()
     serverMode(portn)
